8b020f8af44f299dcd094869dd3fa79a6f6b07e6
trizen <trizenx@gmail.com>
2016-08-18 17:33:51 +0300

    new file:   Math/count_partitions.jl

---
+++ Math/count_partitions.jl
@@ -0,0 +1,36 @@
+#!/usr/bin/julia
+
+# Author: Daniel "Trizen" Șuteu
+# License: GPLv3
+# Date: 18 August 2016
+# Website: https://github.com/trizen
+
+# Count the number of partitions of n.
+
+# See also: https://oeis.org/A000041
+#           https://en.wikipedia.org/wiki/Partition_(number_theory)
+
+function count_partitions(x)
+    n = 2
+    p = [1]
+
+    while (n <= x+1)
+        i = 0
+        q = 2
+        push!(p, 0)
+
+        while q <= n
+            p[n] += (i % 4 > 1 ? -1 : 1) * p[n-q+1]
+            i += 1
+            j = div(i, 2) + 1
+            isodd(i) && (j *= -1)
+            q = div(j * (3j - 1), 2) + 1
+        end
+
+        n += 1
+    end
+
+    p[n-1]
+end
+
+println("P(200) = ", count_partitions(200))        # 3972999029388
--- README.md
+++ README.md
@@ -6,5 +6,7 @@ Small programs written in Julia.
 
 * Image
     * [Julia sets](./Image/julia_sets.jl)
+* Math
+    * [Count partitions](./Math/count_partitions.jl)
 * Text
     * [Smart word wrap](./Text/smart_word_wrap.jl)
