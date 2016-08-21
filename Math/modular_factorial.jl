cccb305eb88b486ec3bd4b082cb5b5d37a82939d
trizen <trizenx@gmail.com>
2016-08-21 21:14:15 +0300

    new file:   Math/modular_factorial.jl

---
+++ Math/modular_factorial.jl
@@ -0,0 +1,22 @@
+#!/usr/bin/julia
+
+# Author: Daniel "Trizen" Șuteu
+# License: GPLv3
+# Date: 21 August 2016
+# Website: https://github.com/trizen
+
+# An efficient algorithm for computing factorial of a large number, modulus a larger number.
+# See: http://stackoverflow.com/questions/9727962/fast-way-to-calculate-n-mod-m-where-m-is-prime
+
+function facmod(n::Int64, mod::Int64)
+
+    f = 1
+    for k in n+1:mod-1
+       f *= k
+       f %= mod
+    end
+
+    (-1 * invmod(f, mod) + mod) % mod
+end
+
+println(facmod(100_000_000, 100_000_037))
--- README.md
+++ README.md
@@ -9,5 +9,6 @@ Small programs written in Julia.
 * Math
     * [Count partitions](./Math/count_partitions.jl)
     * [Count partitions rec](./Math/count_partitions_rec.jl)
+    * [Modular factorial](./Math/modular_factorial.jl)
 * Text
     * [Smart word wrap](./Text/smart_word_wrap.jl)
