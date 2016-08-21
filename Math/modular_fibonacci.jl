bd5e8b3733b87ec80bd72b51bceaf129bf903a6c
trizen <trizenx@gmail.com>
2016-08-22 01:09:42 +0300

    modified:   Math/modular_fibonacci.jl -- added cache

--- Math/modular_fibonacci.jl
+++ Math/modular_fibonacci.jl
@@ -8,21 +8,22 @@
 
 function fibmod(n::Int64, mod::Int64)
 
-    n <  0 && return NaN
-    n == 0 && return 0
-    n == 1 && return 1
+    n <= 1 && return n
+    cache = Dict{Int64, Int64}()
 
     function f(n::Int64)
 
+        n <= 1           && return 1
+        haskey(cache, n) && return cache[n]
+
         k = div(n, 2)
 
-        if n <= 1
-            1
-        elseif n % 2 == 0
-            (f(k) * f(k    ) + f(k - 1) * f(k - 1)) % mod
-        else
-            (f(k) * f(k + 1) + f(k - 1) * f(k    )) % mod
-        end
+        cache[n] =
+            if n % 2 == 0
+                (f(k) * f(k  ) + f(k-1) * f(k-1)) % mod
+            else
+                (f(k) * f(k+1) + f(k-1) * f(k  )) % mod
+            end
     end
 
     f(n-1)
