d4827bdffbc85d9e449578f624d268116246c4f6
trizen <trizenx@gmail.com>
2016-08-27 22:27:11 +0300

    new file:   Math/modular_hyperoperation.jl

---
+++ Math/modular_hyperoperation.jl
@@ -0,0 +1,59 @@
+#!/usr/bin/julia
+
+# Daniel "Trizen" Șuteu
+# Date: 27 August 2016
+# License: GPLv3
+# https://github.com/trizen
+
+# Generalized implementation of Knuth's up-arrow hyperoperation (modulo some n).
+
+# See also:
+#   https://en.wikipedia.org/wiki/Knuth%27s_up-arrow_notation
+
+using Memoize
+
+const modulo = 10^3
+
+@memoize function hyper1(n::Int64, k::Int64)
+    powermod(n, k, modulo)
+end
+
+@memoize function hyper2(n::Int64, k::Int64)
+    k <= 1 && return n
+    hyper1(n, hyper2(n, k - 1))
+end
+
+@memoize function hyper3(n::Int64, k::Int64)
+    k <= 1 && return n
+    hyper2(n, hyper3(n, k - 1))
+end
+
+@memoize function hyper4(n::Int64, k::Int64)
+    k <= 1 && return n
+    hyper3(n, hyper4(n, k - 1))
+end
+
+@memoize function knuth(k::Int64, n::Int64, g::Int64)
+
+    k %= modulo
+    g %= modulo
+
+    n >= 1 && g == 0 && return 1
+
+    n == 0 && return ((k * g) % modulo)
+    n == 1 && return (hyper1(k, g))
+    n == 2 && return (hyper2(k, g))
+    n == 3 && return (hyper3(k, g))
+    n == 4 && return (hyper4(k, g))
+
+    knuth(k, n - 1, knuth(k, n, g - 1))
+end
+
+
+for i in 0:6
+    x = 1 + trunc(Int64, rand()*100)
+    y = 1 + trunc(Int64, rand()*100)
+
+    n = knuth(x, i, y)
+    @printf("%5s %10s %5s = %5s   (mod %s)\n", x, repeat("^", i), y, n, modulo)
+end
--- README.md
+++ README.md
@@ -11,5 +11,6 @@ Small programs written in Julia.
     * [Count partitions rec](./Math/count_partitions_rec.jl)
     * [Modular factorial](./Math/modular_factorial.jl)
     * [Modular fibonacci](./Math/modular_fibonacci.jl)
+    * [Modular hyperoperation](./Math/modular_hyperoperation.jl)
 * Text
     * [Smart word wrap](./Text/smart_word_wrap.jl)
