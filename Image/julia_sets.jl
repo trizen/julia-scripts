030809177d606cb968af5dcba15daebc87991dd6
trizen <trizenx@gmail.com>
2016-12-16 04:04:35 +0200

    modified:   Image/julia_sets.jl

--- Image/julia_sets.jl
+++ Image/julia_sets.jl
@@ -10,14 +10,6 @@
 
 using Images
 
-w, h = 800, 600
-
-zoom = 1
-moveX = 0
-moveY = 0
-
-img = Array(UInt8, h, w, 3)
-
 function hsv2rgb(h, s, v)
     c = v * s
     x = c * (1 - abs(((h/60) % 2) - 1))
@@ -45,28 +37,41 @@ function hsv2rgb(h, s, v)
     r,b,g
 end
 
-for i in 1:100
+function generate()
 
-    maxIter = 50
-    c = Complex(-rand(), 2 * rand() * (rand() < 0.5 ? 1 : -1))
+    w, h = 800, 600
 
-    for x in 1:w
-        for y in 1:h
-            i = maxIter
-            z = Complex(
-                3/2 * (2*x - w) / (w * zoom) + moveX,
-                      (2*y - h) / (h * zoom) + moveY
-            )
-            while abs(z) < 2 && (i -= 1) > 0
-                z = z*z + c
+    zoom = 1
+    moveX = 0
+    moveY = 0
+
+    img = Array(UInt8, h, w, 3)
+
+    for i in 1:100
+
+        maxIter = 50
+        c = Complex(-rand(), 2 * rand() * (rand() < 0.5 ? 1 : -1))
+
+        for x in 1:w
+            for y in 1:h
+                i = maxIter
+                z = Complex(
+                    3/2 * (2*x - w) / (w * zoom) + moveX,
+                          (2*y - h) / (h * zoom) + moveY
+                )
+                while abs(z) < 2 && (i -= 1) > 0
+                    z = z*z + c
+                end
+                r,g,b = hsv2rgb(i / maxIter * 360, 1, i > 0 ? 1 : 0)
+                img[y,x,1] = r
+                img[y,x,2] = g
+                img[y,x,3] = b
             end
-            r,g,b = hsv2rgb(i / maxIter * 360, 1, i > 0 ? 1 : 0)
-            img[y,x,1] = r
-            img[y,x,2] = g
-            img[y,x,3] = b
         end
-    end
 
-    println("Generating image...")
-    save("$c-$zoom.png", colorim(img, "RGB"))
+        println("Generating image...")
+        save("$c-$zoom.png", colorim(img, "RGB"))
+    end
 end
+
+generate()
