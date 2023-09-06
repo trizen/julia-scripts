c6dfe11d424d0389f85bd2f132a8843e20b4501a
trizen <trizenx@tutanota.com>
2023-09-06 18:32:19 +0300

    modified:   Math/sigma_function.jl -- optimized for k=0

--- Math/sigma_function.jl
+++ Math/sigma_function.jl
@@ -12,17 +12,29 @@
 using Primes
 
 function divisor_sum(n, x=1)
+
+    if (x == 0)     # optimization
+        tau = 1
+        for (p,e) in factor(n)
+            tau *= (e+1)
+        end
+        return tau
+    end
+
     sigma = 1
     for (p,e) in factor(n)
         s = 1
+        q = p^x
         for j in 1:e
-            s += p^(j*x)
+            s += q^j
         end
         sigma *= s
     end
-    sigma
+
+    return sigma
 end
 
+println(divisor_sum(5040, 0))     # 60
 println(divisor_sum(1234))        # 1854
 println(divisor_sum(99000))       # 365040
 println(divisor_sum(99000, 2))    # 15359172920
